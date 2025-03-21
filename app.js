5aaf6d53399ca8ee547015c4ae47c9cd73dd71ad
wjddydwns <wjddydwns159@gmail.com>
2025-03-21 18:15:15 +0900

    23

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({extended : false }))
 app.use(bodyParser.json()) // req.body
 app.use("/api",indexRouter)
 
-const mongo_URI  = process.env.LOCAL_DB_ADDRESS
-mongoose.connect(mongo_URI,{useNewUrlParser : true})
+const MONGO_URI  = process.env.LOCAL_DB_ADDRESS
+mongoose.connect(MONGO_URI,{useNewUrlParser : true})
 .then(()=>console.log("mogoose connected"))
 .catch((err)=>console.log("mongoose connected fail",err))
 
