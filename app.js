6a23f444bb64197faccfa3a34a007c9a3893366e
wjddydwns <wjddydwns159@gmail.com>
2025-03-22 10:55:54 +0900

    delete & async

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ app.use(bodyParser.urlencoded({extended : false }))
 app.use(bodyParser.json()) // req.body
 app.use("/api",indexRouter)
 
-const MONGO_URI  = process.env.LOCAL_DB_ADDRESS
-mongoose.connect(MONGO_URI,{useNewUrlParser : true})
+const MONGO_URI  = async() => { process.env.LOCAL_DB_ADDRESS
+await mongoose.connect(MONGO_URI,{useNewUrlParser : true})
 .then(()=>console.log("mogoose connected"))
 .catch((err)=>console.log("mongoose connected fail",err))
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("server on 5001")
 })
+}
 
